5cb919aec08c1a4ad1bbf02378c3b577cf5cefbe
XxxXxxXxx233 <lc18038037737@gmail.com>
2019-12-24 22:30:51 +0800

    py

--- src/serial_writing.py
+++ src/serial_writing.py
@@ -1,8 +1,8 @@
 import serial
 import binascii
 
-ser = serial.Serial('COM5', 115200)
-file = open('coeFiles\\1uart.coe (1).txt')
+ser = serial.Serial('COM6', 115200)
+file = open('coeFiles\\1uart.coe.txt')
 
 string = ''
 for line in file:
